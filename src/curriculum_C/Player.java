f398d1829dd994fd9db30e6755698016c56c7fe2
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-12-03 14:06:23 +0900

    Exceptionエラーでは広すぎるのでIllegalArgumentExceptionに調整

--- src/curriculum_C/Player.java
+++ src/curriculum_C/Player.java
@@ -4,10 +4,10 @@ class Player {
 	private int number;
 	
 	// プレイヤーの手を設定するメソッド
-	public void setNumber(int number) throws Exception {
-		// 0、1、2以外の数値が入力された場合は例外をスロー
-		if (number != 0 && number != 1 && number != 2) {
-			throw new Exception();
+	public void setNumber(int number) {
+		// 0、1、2以外の数値が入力された場合は例外をスロー(int型は整数なので、実質0or1or2かのチェック)
+		if (number < 0 || number > 2) {
+			throw new IllegalArgumentException();
 		}
 		this.number = number;
 	}
