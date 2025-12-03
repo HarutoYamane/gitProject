9f422f47c95fd3b3b9acfd64327553f87d7fc2c2
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-12-03 13:24:56 +0900

    0,1,2以外の数値がnumberに渡されたとき、例外をスローするように修正

--- src/curriculum_C/Player.java
+++ src/curriculum_C/Player.java
@@ -4,7 +4,11 @@ class Player {
 	private int number;
 	
 	// プレイヤーの手を設定するメソッド
-	public void setNumber(int number) {
+	public void setNumber(int number) throws Exception {
+		// 0、1、2以外の数値が入力された場合は例外をスロー
+		if (number != 0 && number != 1 && number != 2) {
+			throw new Exception();
+		}
 		this.number = number;
 	}
 	
